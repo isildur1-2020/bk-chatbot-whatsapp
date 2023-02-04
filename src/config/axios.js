44d693bd0c23208ba5b6c806677c9c44cbed66a8
Esteban G <tapitarias@gmail.com>
2023-02-03 22:31:33 -0500

    add BACK_URL dynamic

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,9 @@
 const axios = require("axios");
 
-const BACK_URL = "http://127.0.0.1:3000/api";
+const BACK_URL =
+  process.env.ENV === "PROD"
+    ? "https://markers-2.herokuapp.com/api"
+    : "http://127.0.0.1:3000/api";
 
 const axiosInstance = axios.create({
   baseURL: BACK_URL,
